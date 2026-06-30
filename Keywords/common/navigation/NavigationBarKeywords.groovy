0f15dab2782ce0a85643ca5f30230766d4024cdb
ououa <1220094688@qq.com>
2026-06-30 10:57:12 +0800

    增加物料归类导航栏封装 Signed-off-by: ououa <1220094688@qq.com>

--- Keywords/common/navigation/NavigationBarKeywords.groovy
+++ Keywords/common/navigation/NavigationBarKeywords.groovy
@@ -3,38 +3,68 @@ package common.navigation
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
 import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.model.FailureHandling
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
-public class NavigationBarKeywords {
+class NavigationBarKeywords {
 
-    @Keyword
-    def openSupplierMaterialMaintenance() {
-        WebUI.click(findTestObject('??_02_???????/Page_- Sun.pro/a_'))
-        WebUI.click(findTestObject('null'))
+    private void clickMenu(String parentObjectId, String childObjectId) {
+        WebUI.waitForElementVisible(findTestObject(parentObjectId), 10)
+        WebUI.waitForElementClickable(findTestObject(parentObjectId), 10)
+        WebUI.click(findTestObject(parentObjectId))
+
+        WebUI.waitForElementVisible(findTestObject(childObjectId), 10)
+        WebUI.waitForElementClickable(findTestObject(childObjectId), 10)
+        WebUI.click(findTestObject(childObjectId))
+
+        WebUI.delay(1)
     }
 
     @Keyword
-    def openMergeRelationshipManagement() {
-        WebUI.callTestCase(findTestCase('Test Cases/P1_Regression_版本回归用例/NavigationBar_导航栏/Common_导航_归并关系管理'), [:])
+    def goto供应商物料维护() {
+        clickMenu(
+            'Object Repository/NavigationBar_导航栏/导航_01_物料主档/Page_- Sun.pro/a__r1t',
+            'Object Repository/NavigationBar_导航栏/导航_02_供应商物料维护/Page_- Sun.pro/a_'
+        )
     }
 
     @Keyword
-    def openClassificationArchive() {
-        WebUI.callTestCase(findTestCase('Test Cases/P1_Regression_版本回归用例/NavigationBar_导航栏/Common_导航_归类档案库'), [:])
+    def goto物料归类申请() {
+        clickMenu(
+            'Object Repository/NavigationBar_导航栏/导航_01_物料主档/Page_- Sun.pro/a__r1t',
+            'Object Repository/NavigationBar_导航栏/导航_03_物料归类申请/Page_- Sun.pro/a__r21'
+        )
     }
 
     @Keyword
-    def openMaterialMasterManagement() {
-        WebUI.callTestCase(findTestCase('Test Cases/P1_Regression_版本回归用例/NavigationBar_导航栏/Common_导航_物料主档管理'), [:])
+    def goto归并关系管理() {
+        clickMenu(
+            'Object Repository/NavigationBar_导航栏/导航_01_物料主档/Page_- Sun.pro/a__r1t',
+            'Object Repository/NavigationBar_导航栏/导航_04_归并关系管理/Page_- Sun.pro/a__r23'
+        )
     }
 
     @Keyword
-    def openMaterialClassificationReview() {
-        WebUI.callTestCase(findTestCase('Test Cases/P1_Regression_版本回归用例/NavigationBar_导航栏/Common_导航_物料归类审核'), [:])
+    def goto物料归类审核() {
+        clickMenu(
+            'Object Repository/NavigationBar_导航栏/导航_01_物料主档/Page_- Sun.pro/a__r1t',
+            'Object Repository/NavigationBar_导航栏/导航_05_物料归类审核/Page_- Sun.pro/a__r25'
+        )
     }
 
     @Keyword
-    def openMaterialClassificationApply() {
-        WebUI.callTestCase(findTestCase('Test Cases/P1_Regression_版本回归用例/NavigationBar_导航栏/Common_导航_物料归类申请'), [:])
+    def goto归类档案库() {
+        clickMenu(
+            'Object Repository/NavigationBar_导航栏/导航_01_物料主档/Page_- Sun.pro/a__r1t',
+            'Object Repository/NavigationBar_导航栏/导航_06_归类档案库/Page_- Sun.pro/a__r27'
+        )
+    }
+
+    @Keyword
+    def goto物料主档管理() {
+        WebUI.waitForElementVisible(findTestObject('Object Repository/NavigationBar_导航栏/导航_01_物料主档/Page_- Sun.pro/a__r1t'), 10)
+        WebUI.waitForElementClickable(findTestObject('Object Repository/NavigationBar_导航栏/导航_01_物料主档/Page_- Sun.pro/a__r1t'), 10)
+        WebUI.click(findTestObject('Object Repository/NavigationBar_导航栏/导航_01_物料主档/Page_- Sun.pro/a__r1t'))
+        WebUI.delay(1)
     }
 }
